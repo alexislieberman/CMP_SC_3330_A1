65dec8be965df8d39c726c5d380f07562c0afc66
Alexis Lieberman <alexislieberman@wi001762fi.dhcp.missouri.edu>
2026-09-23 23:54:26 -0500

    started TicketBook

---
+++ src/tickets/TicketBook.java
@@ -0,0 +1,14 @@
+package tickets;
+
+public class TicketBook {
+	
+	private Ticket[] tickets;
+	private int count;
+	
+	private TicketBook(Ticket[] tickets, int count) {
+		this.tickets = tickets;
+		this.count = count;
+	}
+	
+	
+}
