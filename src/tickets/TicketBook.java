9e0717952a48076d9dcf5ce46eeac901475d178c
Alexis Lieberman <alexislieberman@wi001195fi.dhcp.missouri.edu>
2026-09-24 23:51:05 -0500

    TicketBook class state fixes and TicketManager initial creation

--- src/tickets/TicketBook.java
+++ src/tickets/TicketBook.java
@@ -4,22 +4,28 @@ public class TicketBook {
 	private Ticket[] tickets;
 	private int count;
 	
-	private TicketBook(Ticket[] tickets, int count) {
-		this.tickets = new Ticket[5];
+	public TicketBook(int capacity) {
+		if(capacity <= 0) {
+			throw new IllegalArgumentException("Capacity must be >0");
+		}
+		
+		this.tickets = new Ticket[capacity];
 		this.count = 0;
 	}
 	
-	private void createTicket(int id, Event event, TicketType type, String studentName) {
-		if(this.count >= 5){
+	public Ticket createTicket(int id, Event event, TicketType type, String studentName) {
+		if(this.count >= this.tickets.length){
 			throw new IllegalArgumentException("TicketBook is already full");
 		}
 		
 		Ticket newTicket = new Ticket(id, event, type, studentName);
 		this.tickets[this.count] = newTicket;
 		this.count++;
+		
+		return newTicket;
 	}
 	
-	private Ticket findById(int id) {
+	public Ticket findById(int id) {
 		for (int i = 0; i < this.count; i++) {
 			if(this.tickets[i].getId() == id) {
 				return this.tickets[i];
@@ -29,18 +35,21 @@ public class TicketBook {
 		return null;
 	}
 	
-	private void printAll() {
+	public void printAll() {
 		for (int i = 0; i < this.count; i++) {
-			String toPrint = this.tickets[i].toString();
-			System.out.println(toPrint);
+			System.out.println(this.tickets[i].toString());
 		}
 	}
 	
-	private void printForEvent(Event event) {
+	public void printForEvent(Event event) {
+		if(event==null) {
+			return;
+		}
+		
 		for (int i = 0; i < this.count; i++) {
-			if(this.tickets[i].getEvent() == event) {
-				String toPrint = this.tickets[i].toString();
-				System.out.println(toPrint);
+			//checks equality by reference, not value
+			if(this.tickets[i].getEvent() == event) { 
+				System.out.println(this.tickets[i].toString());
 			}
 		}
 	}
---
+++ src/tickets/TicketManager.java
@@ -0,0 +1,38 @@
+package tickets;
+
+public class TicketManager {
+	private final TicketBook ticketBook;
+	private int nextID = 1;
+	
+	public TicketManager(TicketBook ticketBook) {
+		if(ticketBook==null) {
+			throw new IllegalArgumentException("TicketBook cannot be null");
+		}
+		
+		this.ticketBook = ticketBook;
+	}
+	
+	
+	public Ticket createTicket(Event event, TicketType type, String studentName) {
+		Ticket newTicket = ticketBook.createTicket(this.nextID, event, type, studentName);
+		this.nextID++;
+		return newTicket;
+	}
+	
+	public boolean cancelTicket(int id) {
+		Ticket ticket = ticketBook.findById(id);
+		if(ticket==null) {
+			return false;
+		}
+		return ticket.cancel();
+	}
+	
+	public boolean admitTicket(int id) {
+		Ticket ticket = ticketBook.findById(id);
+		if(ticket==null) {
+			return false;
+		}
+		return ticket.admit();
+	}
+	
+}
